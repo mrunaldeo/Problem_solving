80cdc076bf5435cbe4d05876ed86a947eab39b43
Mrunal Deo <mrunaldeo16@gmail.com>
2024-06-28 18:08:15 +0530

    Create  Second Largest.js

---
+++ Second Largest.js
@@ -0,0 +1,6 @@
+ Second Largest:
+ Problem Description
+You are given an integer array A. You have to find the second largest element/value in the array or report that no such element exists.
+Problem Constraints
+1 <= |A| <= 105
+0 <= A[i] <= 109
