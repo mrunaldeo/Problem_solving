f1e251627a733a16f14d557b1378a932d942ad6b
Mrunal Deo <mrunaldeo16@gmail.com>
2024-08-11 22:00:24 +0530

    Create inorder.java
    1st commit

---
+++ google/Trees/inorder.java
@@ -0,0 +1,14 @@
+class TreeNode{
+  int val;
+  TeeNode left;
+  TreeNode right;
+  TreeNode(){}
+  TreeNode(int val){this.val=val;}
+  TreeNode(int val, TreeNode left,TreeNode right){
+    this.val=val;
+    TreeNode left=left;
+    TreeNode.right=right;
+  }
+}
+class Solution
+  
